4ebe65ddfa79373e6ad0a592816a3ea73831f047
SantiagoLeites <santiagoleitesc@outlook.com>
2022-12-05 04:35:01 -0300

    Delete java_proyecto_metodos/Proyecto directory

--- java_proyecto_metodos/Proyecto/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"px.metodos"},{"l":"px.proyecto"}];updateSearchResults();
--- java_proyecto_metodos/Proyecto/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"px.metodos","l":"metodos"},{"p":"px.proyecto","l":"proyecto_final"}];updateSearchResults();
